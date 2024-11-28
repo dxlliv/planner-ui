7753391fc78e9be4a706d1ef890439201d8d595b
dxlliv <hello@dxlliv.dev>
2024-11-28 11:27:26 +0100

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,26 @@
+import { createResolver } from "@nuxt/kit"
+
+const { resolve } = createResolver(import.meta.url)
+
+export default defineNuxtConfig({
+  app: {
+    head: {
+      meta: [
+        { name: "viewport", content: "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" },
+        { name: "mobile-web-app-capable", content: "yes" },
+        { name: "mobile-web-app-status-bar-style", content: "black-translucent" },
+        {
+          hid: "fragment",
+          name: "fragment",
+          content: "!"
+        }
+      ]
+    }
+  },
+  alias: {
+    "ui": resolve(__dirname, ".")
+  },
+  imports: {
+    dirs: ["composables", "utils"]
+  }
+})
